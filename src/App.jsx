ebc33f5f7abd30d0ad076d349add4b6cdbfb9b60
Ali-Ameer <alialrdeney88@gmail.com>
2023-04-04 15:35:46 +0300

    update code and add some css

--- src/App.jsx
+++ src/App.jsx
@@ -20,6 +20,7 @@ function App() {
     const newRows = rows.filter((row) => row.id !== id);
     setRows(newRows);
   };
+
   const handleInputChange = (event, id) => {
     const { name, value } = event.target;
     const newRows = rows.map((row) =>
@@ -28,87 +29,92 @@ function App() {
     setRows(newRows);
   };
 
-  const dropdownOptions = ['Option 1', 'Option 2', 'Option 3'];
+  const dropdownOptions = ['test data', 'fake data 2', 'some text 3', "one tow three", "just some fake data"];
 
 
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Quantity</th>
-          <th>Storage</th>
-          <th>Row</th>
-          <th>Dixn</th>
-          <th>Actions</th>
-        </tr>
-      </thead>
-      <tbody>
-        {rows.map((row) => (
-          <tr key={row.id}>
-            <td>
-              <input
-                type="text"
-                name="quantity"
-                value={row.quantity}
-                onChange={(e) => handleInputChange(e, row.id)}
-              />
-            </td>
-            <td>
-              <input
-                type="text"
-                name="storage"
-                value={row.storage}
-                onChange={(e) => handleInputChange(e, row.id)}
-                list="storageOptions"
-              />
-              <datalist id="storageOptions">
-                {dropdownOptions.map((option) => (
-                  <option key={option} value={option} />
-                ))}
-              </datalist>
-            </td>
-            <td>
-              <input
-                type="text"
-                name="row"
-                value={row.row}
-                onChange={(e) => handleInputChange(e, row.id)}
-                list="rowOptions"
-              />
-              <datalist id="rowOptions">
-                {dropdownOptions.map((option) => (
-                  <option key={option} value={option} />
-                ))}
-              </datalist>
-            </td>
-            <td>
-              <input
-                type="text"
-                name="dixn"
-                value={row.dixn}
-                onChange={(e) => handleInputChange(e, row.id)}
-                list="dixnOptions"
-              />
-              <datalist id="dixnOptions">
-                {dropdownOptions.map((option) => (
-                  <option key={option} value={option} />
-                ))}
-              </datalist>
-            </td>
-            <td>
-              <button onClick={() => handleDeleteRow(row.id)}>Delete</button>
+    <div className="App">
+      <table>
+        <thead>
+          <tr>
+            <th>Quantity</th>
+            <th>Storage</th>
+            <th>Row</th>
+            <th>Dixon</th>
+          </tr>
+        </thead>
+        <tbody>
+          {rows.map((row) => (
+            <tr key={row.id}>
+              <td>
+                <div className="deleteAndInput">
+                <button onClick={() => handleDeleteRow(row.id)}>
+                  Delete
+                </button>
+                <input
+                  type="text"
+                  name="quantity"
+                  value={row.quantity}
+                  onChange={(e) => handleInputChange(e, row.id)}
+                  />
+                  </div>
+              </td>
+              <td>
+                {/* you can add any value or choose value from dropdown  */}
+                <input type="text"
+                  name="storage"
+                  value={row.storage}
+                  onChange={(e) => handleInputChange(e, row.id)}
+                  list="storageOptions"
+                  autoComplete="on"
+                />
+                <datalist id="storageOptions" >
+                  {dropdownOptions.map((option) => (
+                    <option key={option} value={option} />
+                    ))}
+                </datalist>
+              </td>
+
+              <td>
+                <input
+                  type="text"
+                  name="row"
+                  value={row.row}
+                  onChange={(e) => handleInputChange(e, row.id)}
+                  list="rowOptions"
+                />
+                <datalist id="rowOptions">
+                  {dropdownOptions.map((option) => (
+                    <option key={option} value={option} />
+                  ))}
+                </datalist>
+              </td>
+              <td>
+                <input
+                  type="text"
+                  name="dixn"
+                  value={row.dixn}
+                  onChange={(e) => handleInputChange(e, row.id)}
+                  list="dixnOptions"
+                />
+                <datalist id="dixnOptions">
+                  {dropdownOptions.map((option) => (
+                    <option key={option} value={option} />
+                  ))}
+                </datalist>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+        <tfoot>
+          <tr>
+            <td colSpan="5">
+              <button onClick={handleAddRow}>Add Row</button>
             </td>
           </tr>
-        ))}
-      </tbody>
-      <tfoot>
-        <tr>
-          <td colSpan="5">
-            <button onClick={handleAddRow}>Add Row</button>
-          </td>
-        </tr>
-      </tfoot>
-    </table>
+        </tfoot>
+      </table>
+    </div>
   );
 };
 
